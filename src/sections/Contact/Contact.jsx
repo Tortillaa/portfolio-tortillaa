c6620ba04ae5a10c089174dcbb8a7b75cca24d1e
Tortillaa <aitor.house@hotmail.com>
2025-12-11 00:13:53 +0100

    Tailwind fix & more basic sections

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,16 +1,18 @@
 export default function Contact() {
   return (
     <section id="contact" className="py-24 max-w-3xl mx-auto px-6 text-white">
+    <div className="page-container">
       <h2 className="text-4xl font-bold mb-6">Contacto</h2>
 
       <p className=" mb-4">¿Quieres colaborar o hablar sobre un proyecto?</p>
 
       <a
-        href="mailto:tuemail@correo.com"
+        href="mailto:aitor.house@hotmail.com"
         className="inline-block px-6 py-3 bg-purple-600 rounded-lg text-white mt-4"
       >
         Envíame un email
       </a>
+    </div>
     </section>
   );
 }
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,21 +1,25 @@
 export default function Projects() {
   const projects = [
-    { title: "Proyecto 1", desc: "Descripción", url: "#" },
-    { title: "Proyecto 2", desc: "Descripción", url: "#" },
+    { title: "Silly Gallery", desc: "Just a silly program to practice React Native", url: "https://github.com/Tortillaa/silly-gallery-practice.git" },
+    // No more projects by the moment :(
   ];
 
   return (
     <section id="projects" className="py-24 max-w-5xl mx-auto px-6 text-white">
-      <h2 className="text-4xl font-bold mb-12">Proyectos</h2>
-
-      <div className="grid md:grid-cols-2 gap-8">
-        {projects.map((p) => (
-          <div key={p.title} className="bg-white/10 p-6 rounded-xl backdrop-blur">
-            <h3 className="text-2xl font-bold">{p.title}</h3>
-            <p className="opacity-80 mt-2">{p.desc}</p>
-            <a href={p.url} className="text-purple-400 mt-4 inline-block">Ver más →</a>
-          </div>
-        ))}
+      <div className="page-container">
+        <h2 className="text-4xl font-bold mb-12">Projects</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          {projects.map((p) => (
+            <div key={p.title} className="bg-white/10 p-6 rounded-xl backdrop-blur">
+              <h3 className="text-2xl font-bold">{p.title}</h3>
+              <p className="opacity-80 mt-2">{p.desc}</p>
+              <a href={p.url} className="text-purple-400 mt-4 inline-block">Link to repo →</a>
+            </div>
+          ))}
+          <br />
+          <br />
+          <h4 className="text-2xl font-bold">More coming soon...</h4>
+        </div>
       </div>
     </section>
   );
